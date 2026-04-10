9c9eb08cf59265b8e132fa60e8781af7bf318da0
zohinur miah <zmiah@my.yearupunited.org>
2026-04-10 15:03:00 -0400

    new

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Declaring the variables
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter you first number: ");
+        int firstNum = myScanner.nextInt();
+        System.out.print("Enter your second number: ");
+        int secondNum = myScanner.nextInt();
+
+        System.out.print("Possible Calculations ");
+        String character2 = myScanner.next();
+
+
+        System.out.println("Here is the result");
+        //Adding the conditions
+        if (character2.equals("A")) {
+            int finalNum = firstNum + secondNum;
+            System.out.println("Your new number is: " +finalNum);
+        } else if (character2.equals("S")) {
+            int subtractNum = firstNum - secondNum;
+            System.out.println("Subtracting it results in:" + subtractNum);
+        } else if (character2.equals("M")) {
+            int multiplyNum = firstNum * secondNum;
+            System.out.println("Multiplying the number is:" + multiplyNum);
+        } else if (character2.equals("D")) {
+            int dividNum = firstNum/secondNum;
+            System.out.println("Dividing the number is: " + dividNum);
+        } else {
+            System.out.println("You need to enter a Character above!!");
+        }
+    }
 }
--- src/main/java/com/pluralsight/PayRollCalculator.java
+++ src/main/java/com/pluralsight/PayRollCalculator.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayRollCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String name = myScanner.nextLine();
+        System.out.print("Enter how many hours you worked: ");
+        float workedHours = myScanner.nextFloat();
+        System.out.print("What is your pay rate: ");
+        float payRate = myScanner.nextFloat();
+        if( workedHours > 40) {
+            double extraHours = workedHours - 40;
+            double overTime = extraHours * payRate * 1.5;
+            double regularPay = 40 * payRate;
+            double OverallPaid = overTime + regularPay;
+            System.out.println("Your over time pay is: " + OverallPaid);
+        } else {
+
+           double paytime = workedHours * payRate;
+            System.out.println("Here is ther regular pay: " + paytime);
+        }
+    }
 }
